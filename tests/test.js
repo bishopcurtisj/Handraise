231ad292260b9b95ffb7a2caced27259f6f30dec
bishopcurtisj <123041734+bishopcurtisj@users.noreply.github.com>
2024-12-01 21:27:15 -0700

    Delete tests directory

--- tests/test.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-console.log("PRIVATE_KEY:", process.env.PRIVATE_KEY);
-console.log("SEPOLIA_RPC_URL:", process.env.SEPOLIA_RPC_URL);
-console.log("TEST_VAR:", process.env.TEST_VAR);
--- tests/testProvider.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-
-const provider = new HDWalletProvider(
-    process.env.PRIVATE_KEY,
-    process.env.SEPOLIA_RPC_URL
-);
-
-console.log("Provider initialized:", provider);
